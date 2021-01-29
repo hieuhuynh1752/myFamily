3a5d796895579ece629d9facd792137a07ec4ad8
thaihieuhuynh1752 <thaihieuhuynh1752@gmail.com>
2021-01-29 10:46:06 +0700

    Done Graphql-server config for Posts/Comments/Likes CRUD

--- server/app/Models/Post.php
+++ server/app/Models/Post.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Post extends Model
 {
@@ -13,5 +14,14 @@ class Post extends Model
     public function familyMember(): BelongsTo
     {
         return $this->belongsTo(FamilyMember::class, 'member_id');
+    }
+    public function comment(): HasMany
+    {
+        return $this->hasMany(Comment::class, 'post_id');
+    }
+
+    public function like(): HasMany
+    {
+        return $this->hasMany(Like::class, 'post_id');
     }
 }
