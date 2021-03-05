8659df94c82085437f9df80639b74af57bd88219
thaihieuhuynh1752 <thaihieuhuynh1752@gmail.com>
2021-03-05 08:44:32 +0700

    Setup ManageScreen

---
+++ client/screens/ManageScreen.js
@@ -0,0 +1,8 @@
+import React, {useState} from 'react';
+import {Text} from 'react-native-paper';
+
+const ManageScreen = ({navigation}) =>{
+    return <Text>This is Manage Screen</Text>
+}
+
+export default ManageScreen;
